d1ef0635868f3b4d3e432bc69c6ffd29625891d8
rkatz36368 <rkatz36368@gmail.com>
2022-07-02 00:39:23 -0300

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+  <GmapMap
+    :center="center"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+  >
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+  />
+</GmapMap>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+          center: {
+            lat: 10.0,
+            lng: 10.0
+          },
+          markers: [{
+            position: {
+              lat: 10.0,
+              lng: 10.0
+            }
+          }, {
+            position: {
+              lat: 11.0,
+              lng: 11.0
+            }
+          }]       
+            }
+}
+}
+</script>
